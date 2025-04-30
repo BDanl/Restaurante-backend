6c89bf5031dd680f0e586b10d280c46f36084110
BDanl <alejandrobovea@gmail.com>
2025-04-30 03:27:08 -0500

    format

--- controllers/test/eliminar.js
+++ controllers/test/eliminar.js
@@ -1,9 +1,9 @@
 // DELETE: Eliminar un recurso
 const eliminarDato = (req, res) => {
-    const { id } = req.params; // Ejemplo: ID del recurso a eliminar
-    res.json({ 
-      mensaje: `Dato con ID ${id} eliminado correctamente` 
-    });
-  };
-  
-  module.exports = { eliminarDato };
+  const { id } = req.params; // Ejemplo: ID del recurso a eliminar
+  res.json({
+    mensaje: `Dato con ID ${id} eliminado correctamente`,
+  });
+};
+
+module.exports = { eliminarDato };
